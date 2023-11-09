5a9f1b118af096fc175c90fdc5769086ba8deebc
Guru <shubhamka13@gmail.com>
2023-11-09 17:41:32 +0530

    LoginPage Design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
+
+import {LoginPage} from "./Routes.js";
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
   return (
     <>
     
-    Hello World!
+    <Router>
+      <Routes>
+        <Route exact path='/login' element={<LoginPage />} />
+      </Routes>
+    </Router>
     
     </>
   );
